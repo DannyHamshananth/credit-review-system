48c99733e5a9bae2acc53de5214807943ffb0ac4
Danny Hamshananth <hamshananth@gmail.com>
2025-09-10 17:54:39 +0530

    Working Pipeline card component

---
+++ components/CardPipeline.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardContent } from './ui/card';
+import Pipelineprops from "@/lib/Pipelineprops";
+
+export default function CardPipeline({ id, name, loan_type, amount, onClick }: Pipelineprops) {
+    return (
+        <Card key={id} className="p-4 min-w-fit">
+            <CardContent className="flex justify-between items-center cursor-pointer transition-transform transform hover:scale-105" onClick={onClick}>
+                <div>
+                    <p className="font-medium">{name}</p>
+                    <p className="text-sm text-muted-foreground">{loan_type}</p>
+                </div>
+                <p className="font-semibold">{amount}</p>
+            </CardContent>
+        </Card>
+    );
+};
