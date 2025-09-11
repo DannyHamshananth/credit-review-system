4521934101cbb637d1b59ff8f55c683b878dcdbc
Danny Hamshananth <hamshananth@gmail.com>
2025-09-11 21:54:08 +0530

    Added status in Pipeline Card

--- components/CardPipeline.tsx
+++ components/CardPipeline.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Card, CardContent } from './ui/card';
 import Pipelineprops from "@/lib/Pipelineprops";
 
-export default function CardPipeline({ id, name, loan_type, amount, onClick }: Pipelineprops) {
+export default function CardPipeline({ id, name, loan_type, status, amount, onClick }: Pipelineprops) {
     return (
         <Card key={id} className="p-4 min-w-fit">
             <CardContent className="flex justify-between items-center cursor-pointer transition-transform transform hover:scale-105" onClick={onClick}>
                 <div>
                     <p className="font-medium">{name}</p>
                     <p className="text-sm text-muted-foreground">{loan_type}</p>
+                    <p className="text-sm text-muted-foreground">{status}</p>
                 </div>
                 <p className="font-semibold">{amount}</p>
             </CardContent>
